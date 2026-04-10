f63d154bbb78f563a065b15932e2a74c951ab35b
nithyarubini0112 <nithyarubinir@karunya.edu.in>
2026-04-10 19:23:47 +0530

    Full DevOps pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,76 +2,8 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-# -----------------------------
-# Deployment
-# -----------------------------
-resource "kubernetes_deployment" "hostel_app" {
+resource "kubernetes_namespace" "hostel" {
   metadata {
-    name = "hostel-app"
-    labels = {
-      app = "hostel"
-    }
+    name = "hostel-namespace"
   }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "hostel"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "hostel"
-        }
-      }
-
-      spec {
-        container {
-          name  = "hostel-container"
-          image = "hostel-app"
-
-          # IMPORTANT for Minikube local image
-          image_pull_policy = "Never"
-
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
-
-# -----------------------------
-# Service (to expose app)
-# -----------------------------
-resource "kubernetes_service" "hostel_service" {
-  metadata {
-    name = "hostel-service"
-  }
-
-  spec {
-    selector = {
-      app = "hostel"
-    }
-
-    port {
-      port        = 80
-      target_port = 8080
-      node_port   = 30007
-    }
-
-    type = "NodePort"
-  }
-}
-
-# -----------------------------
-# Output
-# -----------------------------
-output "app_info" {
-  value = "Hostel app deployed using Terraform (IaC) 🚀"
 }
